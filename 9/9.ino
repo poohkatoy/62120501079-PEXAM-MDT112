97fec01277e2b6ae7caa96211bd1c1c7591a5717
poohkatoy <katoy1956@gmail.com>
2020-07-04 15:45:18 +0700

    edit9

---
+++ 9/9.ino
@@ -0,0 +1,25 @@
+#include <Stepper.h>
+#include "TM1637.h"
+
+const int stepsPerRevolution = 200;  
+const int CLK = 17;
+const int DIO = 16;
+
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+TM1637 sevenSegment(CLK, DIO);
+
+int stepCount = 0;       
+
+void setup() {
+  sevenSegment.init();
+  sevenSegment.set(7);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  myStepper.step(1);
+  sevenSegment.delayNum(stepCount);
+  stepCount++;
+  delay(500);
+}
